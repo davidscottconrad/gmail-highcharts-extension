0e9f5e807bfb3f55b4b75c7837f38728f5434cb1
davidscottconrad <60311537+davidscottconrad@users.noreply.github.com>
2025-03-29 20:58:28 -0400

    Working html

--- src/content.js
+++ src/content.js
@@ -5,17 +5,24 @@ InboxSDK.load(2, "sdk_DavidConradTest_305edd048f").then((sdk) => {
     composeView.addButton({
       title: "New Pie Chart",
       iconUrl: "https://cdn-icons-png.flaticon.com/128/12461/12461700.png",
-      onClick(event) {
-        const { el, textarea } = createPopupContent();
+      async onClick(event) {
+
+        const html = await fetch(
+          chrome.runtime.getURL("drawer.html"))
+            .then(res => res.text());
+
+        const tempDiv = document.createElement('div');
+        tempDiv.innerHTML = html;
 
         const modal = sdk.Widgets.showModalView({
           title: "New Pie Chart",
-          el: el,
+          el: tempDiv,
           buttons: [
             {
               text: "Save",
               onClick: function () {
-                const userText = textarea.value;
+                const textarea = tempDiv.querySelector("#extra-info");
+                const userText = textarea?.value || "";
                 event.composeView.insertTextIntoBodyAtCursor(userText);
                 modal.close();
               }
@@ -32,21 +39,3 @@ InboxSDK.load(2, "sdk_DavidConradTest_305edd048f").then((sdk) => {
     });
   });
 });
-
-function createPopupContent() {
-  const div = document.createElement('div');
-  div.style.padding = "2rem";
-
-  const label = document.createElement('label');
-  label.textContent = "Additional Info:";
-
-  const textarea = document.createElement('textarea');
-  textarea.style.width = "100%";
-  textarea.style.height = "100px";
-
-  div.appendChild(label);
-  div.appendChild(document.createElement('br'));
-  div.appendChild(textarea);
-
-  return { el: div, textarea };
-}
